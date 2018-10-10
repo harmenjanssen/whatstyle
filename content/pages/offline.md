8bc51fe43704a842db4a41b5a13502486ccdac07
Harmen Janssen <harmen@grrr.nl>
2018-10-11 00:31:03 +0200

    Add offline page

---
+++ content/pages/offline.md
@@ -0,0 +1,9 @@
+---
+url: /offline
+title: Oh geez. You're offline.
+---
+
+Looks like your internet wire is not really plugged into your internet socket 🤓 ...or something.
+
+
+
--- themes/whatstyle/static/serviceworker.js
+++ themes/whatstyle/static/serviceworker.js
@@ -1,59 +1,68 @@
 /**
- * Service Worker V0.01
+ * Service Worker
  *
- * Provide some simple offline handling and caching.
- *
- * HTML files: try the network first, then the cache.
- * Other files: try the cache first, then the network.
- * Both: cache a fresh version if possible.
- *
- * Adapted from Adactio: https://gist.github.com/adactio/3717b7da007a9363ddf21f584aae34af
+ * Provides offline fallback and caching.
+ * We always try the cache first, then the network. Fresh responses are cached asynchronously.
  */
-const version = 'V0.02';
-const staticCacheName = 'static';
+const version = 'V0.09';
+const staticCacheName = `${version}::static`;
 
 // Install
-//addEventListener('install', installEvent => {
-  //installEvent.waitUntil(
-    //caches.open(staticCacheName)
-      //.then(staticCache => {
-        //return staticCache.addAll([
-          //'/css/styles.css',
-          //'/js/main.js',
-          ////'/offline'
-        //]);
-      //})
-  //);
-//});
+addEventListener('install', installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticCacheName)
+      .then(staticCache => {
+        return staticCache.addAll([
+          '/',
+          '/offline/',
+          '/css/styles.css',
+          '/js/main.js',
+        ]);
+      })
+  );
+});
 
 // Fetch
 addEventListener('fetch', fetchEvent => {
   const request = fetchEvent.request;
-  if (request.method !== 'GET') {
+  const url = new URL(request.url);
+
+  if (request.method !== 'GET' || url.origin !== location.origin) {
+    // Skip any POST requests, or requests to other domains.
     return;
   }
-  const url = new URL(request.url);
-  if (url.origin !== location.origin) {
-      return;
-  }
-  fetchEvent.respondWith(async function() {
-    const fetchPromise = fetch(request);
-    fetchEvent.waitUntil(async function() {
-      const responseFromFetch = await fetchPromise;
-      const responseCopy = responseFromFetch.clone();
-      const myCache = await caches.open(staticCacheName);
-      return myCache.put(request, responseCopy);
-    }());
-    if (request.headers.get('Accept').includes('text/html')) {
-      try {
-        return fetchPromise;
-      }
-      catch(error) {
-        return caches.match(request) || caches.match('/offline');
-      }
-    } else {
-      const responseFromCache = await caches.match(request);
-      return responseFromCache || fetchPromise;
-    }
-  }());
+  fetchEvent.respondWith(
+    // Check the cache first.
+    caches.match(request)
+      .then(responseFromCache => {
+        if (responseFromCache) {
+          // Store the fresh version in the cache asynchronously.
+          fetchEvent.waitUntil(
+            fetch(request)
+              .then(responseFromFetch =>
+                caches.open(staticCacheName)
+                  .then(cache => cache.put(request, responseFromFetch))
+              )
+          );
+          return responseFromCache;
+        }
+        // Check the network.
+        return fetch(request)
+          .then(responseFromFetch => {
+            const clonedResponse = responseFromFetch.clone();
+            // Store the fresh version in the cache asynchronously.
+            fetchEvent.waitUntil(
+              caches.open(staticCacheName)
+                .then(cache => cache.put(request, clonedResponse))
+            );
+            return responseFromFetch;
+          })
+          .catch(() => {
+            // Offline!
+            if (request.headers.get('Accept').includes('text/html')) {
+              return caches.match('/offline/');
+            }
+          });
+      })
+  );
 });

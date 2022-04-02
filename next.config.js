a16e82538b2d4833290275d2eef9f41a78803c4c
Harmen Janssen <harmen@grrr.nl>
2022-04-02 19:44:20 +0200

    Add lang attribute to HTML element

--- next.config.js
+++ next.config.js
@@ -3,5 +3,11 @@ const withPWA = require("next-pwa");
 module.exports = withPWA({
   pwa: {
     dest: "public",
+    disable: process.env.NODE_ENV === "development",
+  },
+  // This is added to ensure a `lang` attribute is added to the `<html>` tag.
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
   },
 });
